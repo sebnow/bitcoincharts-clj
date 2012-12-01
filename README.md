1723a33f64abda965923930e9c8bdefa4f3f2712
Sebastian Nowicki <sebnow@gmail.com>
2012-12-01 13:34:41 +0800

    Add project metadata

---
+++ README.md
@@ -0,0 +1,16 @@
+# BitcoinCharts
+
+A Clojure library which abstracts the [BitcoinCharts API](api) API.
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2012 Sebastian Nowicki
+
+Distributed under the MIT License.
+
+[api]: http://bitcoincharts.com/about/markets-api
+
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject bitcoincharts "0.1.0-SNAPSHOT"
+  :description "BitcoinCharts API"
+  :url "http://github.com/sebnow/bitcoincharts-clj"
+  :license {:name "MIT"
+            :url "http://www.opensource.org/licenses/mit-license.php"}
+  :dependencies [[org.clojure/clojure "1.4.0"]])
+
